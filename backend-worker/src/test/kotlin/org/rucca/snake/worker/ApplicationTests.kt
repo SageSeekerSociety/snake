3edd7ed80dc992a6260a8812e60370d93feceaed
nictheboy <nictheboy@outlook.com>
2025-01-20 21:27:36 +0800

    feat: init spring boot project

---
+++ backend-worker/src/test/kotlin/org/rucca/snake/worker/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rucca.snake.worker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
