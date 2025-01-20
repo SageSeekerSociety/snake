b77b4f49190e55b94ce3a77c77de98ff228af4ae
nictheboy <nictheboy@outlook.com>
2025-01-21 00:29:34 +0800

    regenerate code

--- backend-controller/src/main/kotlin/org/rucca/snake/controller/controller/SubmitController.kt
+++ backend-controller/src/main/kotlin/org/rucca/snake/controller/controller/SubmitController.kt
@@ -2,13 +2,13 @@ package org.rucca.snake.controller.controller
 
 import org.rucca.snake.controller.api.SubmitApi
 import org.rucca.snake.controller.model.SubmitPost200ResponseDTO
-import org.springframework.core.io.Resource
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class SubmitController : SubmitApi {
-    override fun submitPost(src: Resource?): ResponseEntity<SubmitPost200ResponseDTO> {
+    override fun submitPost(src: MultipartFile?): ResponseEntity<SubmitPost200ResponseDTO> {
         return super.submitPost(src)
     }
 }
